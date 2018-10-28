ab1409fba4556bf07f79263b97dfe714c56d0e3f
BVMiko <BVMiko@gmail.com>
2018-10-27 20:51:17 -0500

    Fix ability to send individual commands and add logging information

--- hisense.py
+++ hisense.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 from socket import *
 from select import select
@@ -11,6 +11,7 @@ class CM(object):
 		self.host = host
 		self.inputs = inputs
 		self.outputs = outputs
+		print('cm_open_connection: ', host)
 		self.sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)
 		self.sock.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
 		self.sock.setblocking(0)
@@ -20,6 +21,7 @@ class CM(object):
 
 	def write(self):
 		if self.state == 0:
+			print('cm_send: ', b'CTCREATE\r\nID: 93901743\r\n\r\n\0')
 			self.sock.sendto(b'CTCREATE\r\nID: 93901743\r\n\r\n\0', self.host)
 			self.outputs.remove(self)
 			self.state += 1
@@ -44,6 +46,7 @@ class CT(object):
 		self.host = host
 		self.inputs = inputs
 		self.outputs = outputs
+		print('ct_open_connection: ', host)
 		self.sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)
 		self.sock.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
 		self.sock.setblocking(0)
@@ -53,13 +56,16 @@ class CT(object):
 
 	def write(self):
 		if self.state == 0:
+			print('ct_send: ', b'SUS\0')
 			self.sock.sendto(b'SUS\0', self.host)
 			self.state += 1
 		elif self.state == 1:
+			print('ct_send: ', b'CTCREATE\r\nMAC: appmac_appmac_app\r\nVERSION: 0001\r\n\r\n\0')
 			self.sock.sendto(b'CTCREATE\r\nMAC: appmac_appmac_app\r\nVERSION: 0001\r\n\r\n\0', self.host)
 			self.outputs.remove(self)
 			self.state += 1
 		elif self.state == 3:
+			print('ct_send: ', b'CCCREATE\r\nID: 93901743\r\n\r\n\0')
 			self.sock.sendto(b'CCCREATE\r\nID: 93901743\r\n\r\n\0', self.host)
 			self.outputs.remove(self)
 			self.state += 1
@@ -76,7 +82,6 @@ class CT(object):
 				self.state += 1
 			elif self.state == 4:
 				ccportnum = int(data.rstrip('\0\r\n').split('\r\n')[1].split(':')[1].strip(' '))
-				print ('creating new cc: ', ccportnum)
 				CC((self.host[0], ccportnum), self.inputs, self.outputs)
 				self.state += 1
 
@@ -85,52 +90,53 @@ class CT(object):
 
 class CC(object):
 	keyloop = 0
-	keylist = (
-		b'KEY_VOLUMEUP', \
-		b'KEY_INPUT', \
-		b'KEY_INPUT1', \
-		b'KEY_INPUT_1', \
-		b'KEY_INPUT2', \
-		b'KEY_INPUT_2', \
-		b'KEY_MODE', \
-		b'KEY_MODE1', \
-		b'KEY_MODE_1', \
-		b'KEY_MODE2', \
-		b'KEY_MODE_2', \
-		b'KEY_SOURCE', \
-		b'KEY_SOURCE1', \
-		b'KEY_SOURCE_1', \
-		b'KEY_SOURCE2', \
-		b'KEY_SOURCE_2', \
-		b'KEY_HDMI', \
-		b'KEY_HDMI1', \
-		b'KEY_HDMI_1', \
-		b'KEY_HDMI2', \
-		b'KEY_HDMI_2', \
-		b'KEY_LIVETV', \
-		b'KEY_LIVE_TV', \
-		b'KEY_LIVE', \
-		b'KEY_TVLIVE', \
-		b'KEY_TV_LIVE', \
-		b'KEY_MUTE', \
-		b'KEY_MUTING', \
-		b'KEY_SLEEPTIMER', \
-		b'KEY_TIMER', \
-		b'KEY_TIMEOUT', \
-		b'KEY_TIMEOFF', \
-		b'KEY_APP', \
-		b'KEY_APPS', \
-		b'KEY_APPLICATIONS', \
-		b'KEY_PROG', \
-		b'KEY_PROGS', \
-		b'KEY_PROGRAMS', \
-		b'KEY_FN')
+	# keylist = (
+	# 	b'KEY_VOLUMEUP', \
+	# 	b'KEY_INPUT', \
+	# 	b'KEY_INPUT1', \
+	# 	b'KEY_INPUT_1', \
+	# 	b'KEY_INPUT2', \
+	# 	b'KEY_INPUT_2', \
+	# 	b'KEY_MODE', \
+	# 	b'KEY_MODE1', \
+	# 	b'KEY_MODE_1', \
+	# 	b'KEY_MODE2', \
+	# 	b'KEY_MODE_2', \
+	# 	b'KEY_SOURCE', \
+	# 	b'KEY_SOURCE1', \
+	# 	b'KEY_SOURCE_1', \
+	# 	b'KEY_SOURCE2', \
+	# 	b'KEY_SOURCE_2', \
+	# 	b'KEY_HDMI', \
+	# 	b'KEY_HDMI1', \
+	# 	b'KEY_HDMI_1', \
+	# 	b'KEY_HDMI2', \
+	# 	b'KEY_HDMI_2', \
+	# 	b'KEY_LIVETV', \
+	# 	b'KEY_LIVE_TV', \
+	# 	b'KEY_LIVE', \
+	# 	b'KEY_TVLIVE', \
+	# 	b'KEY_TV_LIVE', \
+	# 	b'KEY_MUTE', \
+	# 	b'KEY_MUTING', \
+	# 	b'KEY_SLEEPTIMER', \
+	# 	b'KEY_TIMER', \
+	# 	b'KEY_TIMEOUT', \
+	# 	b'KEY_TIMEOFF', \
+	# 	b'KEY_APP', \
+	# 	b'KEY_APPS', \
+	# 	b'KEY_APPLICATIONS', \
+	# 	b'KEY_PROG', \
+	# 	b'KEY_PROGS', \
+	# 	b'KEY_PROGRAMS', \
+	# 	b'KEY_FN')
 
 
 	def __init__(self, host, inputs, outputs):
 		self.host = host
 		self.inputs = inputs
 		self.outputs = outputs
+		print('cc_open_connection:', host)
 		self.sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)
 		self.sock.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
 		self.sock.setblocking(0)
@@ -140,22 +146,25 @@ class CC(object):
 
 	def write(self):
 		if self.state == 0:
+			print ('cc_send: ', b'SUS\0')
 			self.sock.sendto(b'SUS\0', self.host)
 			self.state += 1
 		elif self.state == 1:
 
+			# This should be handled more gracefully...
+			opts, args = getopt(sys.argv[1:], "ld:", ["list", "device="])
 
-			key = self.keylist[self.keyloop]
-			print('key: ', key)
+			if (self.keyloop >= len(args)):
+				print('no further input, exiting')
+				sys.exit(0)
+			key = args[self.keyloop]
 			mylen = len(key) + 129
 			mylenstr = str(mylen).zfill(6).encode()
+			print('cc_send: ', b'CMD %s\r\n1\r\n1HISENSE_DELIMITER2HISENSE_DELIMITER2HISENSE_DELIMITER%sHISENSE_DELIMITER10HISENSE_DELIMITER0HISENSE_DELIMITER0\r\n\r\n\0' % (mylenstr, key))
 			self.sock.sendto(b'CMD %s\r\n1\r\n1HISENSE_DELIMITER2HISENSE_DELIMITER2HISENSE_DELIMITER%sHISENSE_DELIMITER10HISENSE_DELIMITER0HISENSE_DELIMITER0\r\n\r\n\0' % (mylenstr, key), self.host)
-			# self.sock.sendto(b'CMD 000143\r\n1\r\n1HISENSE_DELIMITER2HISENSE_DELIMITER2HISENSE_DELIMITERKEY_VOLUMEDOWNHISENSE_DELIMITER10HISENSE_DELIMITER0HISENSE_DELIMITER0\r\n\r\n\0', self.host)
-			# self.sock.sendto(b'CMD 000143\r\n1\r\n1HISENSE_DELIMITER2HISENSE_DELIMITER2HISENSE_DELIMITERKEY_VOLUMEDOWNHISENSE_DELIMITER10HISENSE_DELIMITER0HISENSE_DELIMITER0\r\n\r\n\0', self.host)
-			# self.sock.sendto(b'CMD 000143\r\n1\r\n1HISENSE_DELIMITER2HISENSE_DELIMITER2HISENSE_DELIMITERKEY_VOLUMEDOWNHISENSE_DELIMITER10HISENSE_DELIMITER0HISENSE_DELIMITER0\r\n\r\n\0', self.host)
 
 			self.keyloop += 1
-			sleep(0.5)
+			# sleep(0.5)
 			# self.outputs.remove(self)
 			# self.state += 1
 		elif self.state == 3:
@@ -230,8 +239,8 @@ class HisenseTV(object):
 
 def usage():
 	print ("-l (--list):	Send a broadcast to discover available devices\n")
-	print ("-d (--device):	Pass specific device IP address (useful if more than one on the network)\n")
-	print ("any other input is considered a command to send to the TV\n")
+	print ("-d (--device):	[INCOMPLETE] Pass specific device IP address (useful if more than one on the network)\n")
+	print ("any other input(s) are considered commands to send to the TV\n")
 	sys.exit()
 
 def main(argv):
@@ -241,6 +250,7 @@ def main(argv):
 	except GetoptError:
 		usage()
 		sys.exit(2)
+
 	for opt, arg in opts:
 		if opt in ("-l", "--list"):
 			print(HisenseTV.discover())
@@ -262,9 +272,7 @@ def main(argv):
 	CM((device, 60030), inputs, outputs)
 
 	while inputs:
-		# print ('preloop')
 		readable, writable, exceptional = select(inputs, outputs, inputs)
-		print ('postloop')
 		for s in readable:
 			# print('readable', s)
 			s.read()
